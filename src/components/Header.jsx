c6e879830d498a598ef5d3ffa37dccd3416b4745
sohilakaraly340 <sohilakaraly958@gmail.com>
2024-06-20 19:31:34 -0700

    add search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,25 @@ import React from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
 
-export default function Header({ title, buttonText, route }) {
+export default function Header({
+  title,
+  buttonText,
+  route,
+  searchTerm = "",
+  handleSearch = null,
+}) {
   return (
-    <div className="flex justify-between items-center ">
-      <p className="text-2xl font-bold ">{title}</p>
+    <div className="flex justify-between items-center">
+      <p className="text-2xl font-bold">{title}</p>
+      {title === "All Items" && (
+        <input
+          type="text"
+          placeholder={`Search ...`}
+          value={searchTerm}
+          onChange={handleSearch}
+          className="w-1/2 p-2 border border-[#E3D5CA] rounded-md"
+        />
+      )}
       {route ? (
         <Link to={route}>
           <Button text={buttonText} />
